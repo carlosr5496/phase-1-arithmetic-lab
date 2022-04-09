0316fa8a64a2cb3b880f3c11eb8259b4b5b1169c
Ryan Carlos <ryancarlos925@gmail.com>
2022-04-09 14:26:22 -0700

    commit tree bd62717b250a6bfba9158445e97b468a4c921557

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 22;
+let num4 = 6;
+let result = num3 / num4 ;
+let mod = Math.round(result);
+
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
